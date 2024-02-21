3074576f68aa109b2e7c046203be49dbd4c704ef
ManuelaGouveiaa <manuelagouveia@ua.pt>
2024-02-21 13:00:17 +0000

    Construction of the "Bolodecenoura.jsx" component, where a list was created. The component was imported into "App.jsx".

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Bolodecenoura from './Bolodecenoura';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Bolodecenoura></Bolodecenoura>
+    </div>
+  );
+}
+
+export default App;
