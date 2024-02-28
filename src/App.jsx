f05e3d556582b3cceacd120b59efa256073462a8
ManuelaGouveiaa <manuelagouveia@ua.pt>
2024-02-28 11:57:11 +0000

    Updates on the following pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Bolodecenoura from './Bolodecenoura';
+import logo from "./logo.svg";
+import "./App.css";
+import Bolodecenoura from "./Bolodecenoura";
 
 function App() {
   return (
     <div className="App">
-      
       <Bolodecenoura></Bolodecenoura>
     </div>
   );
